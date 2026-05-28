41ba4c137cbf3bb8dac46ac58815044841b4d21f
yolbisadrian <soyyolbisadrianreinoza@gmail.com>
2026-05-28 19:08:17 -0400

    Remove Google Sheets link from connect_gsheet
    Removed a direct link to a Google Sheets document from the connect_gsheet function.

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def connect_gsheet():
     creds_dict = json.loads(st.secrets["gcp_service_account"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-    https://docs.google.com/spreadsheets/d/1kXr2RMPumkkKhhxrv0jTSPSDSp7C4t1ctV7M_WIU2Jg/edit?usp=sharing
     return client.open_by_key("1kXr2RMPumkkHhhxrv0jTSPSDSp7C4t1ctV7M_WIU2jg").sheet1
 
 st.title("Sistema de Incidencias de Inventario")
